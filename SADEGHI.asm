28d569e03f34407bf10cc005d5feea1372393214
arina <49390842+arinaskh@users.noreply.github.com>
2021-11-29 22:28:33 +0330

    Add files via upload

---
+++ SADEGHI.asm
@@ -0,0 +1,1629 @@
+mov AH, 02h
+mov BH, 00
+mov DH, 22
+mov DL, 030
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00001001b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 22
+mov DL, 031
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00001001b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 22
+mov DL, 032
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00001001b
+mov cx,1
+int 10h 
+        
+mov AH, 02h
+mov BH, 00
+mov DH, 22
+mov DL, 033
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00001001b
+mov cx,1
+int 10h        
+       
+mov AH, 02h
+mov BH, 00
+mov DH, 22
+mov DL, 034
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00001001b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 22
+mov DL, 035
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00001001b
+mov cx,1
+int 10h 
+
+mov AH, 02h
+mov BH, 00
+mov DH, 22
+mov DL, 036
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00001001b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 22
+mov DL, 037
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00001001b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 22
+mov DL, 038
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00001001b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 22
+mov DL, 039
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00001001b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 22
+mov DL, 040
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00001001b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 22
+mov DL, 041
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00001001b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 22
+mov DL, 042
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00001001b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 22
+mov DL, 043
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00001001b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 22
+mov DL, 044
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00001001b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 22
+mov DL, 045
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00001001b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 21
+mov DL, 030
+int 10h
+mov ah,09
+mov al,7CH
+mov bh,00
+mov bl,00001001b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 20
+mov DL, 030
+int 10h
+mov ah,09
+mov al,7CH
+mov bh,00
+mov bl,00001001b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 19
+mov DL, 030
+int 10h
+mov ah,09
+mov al,7CH
+mov bh,00
+mov bl,00001001b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 18
+mov DL, 030
+int 10h
+mov ah,09
+mov al,7CH
+mov bh,00
+mov bl,00001001b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 17
+mov DL, 030
+int 10h
+mov ah,09
+mov al,7CH
+mov bh,00
+mov bl,00001001b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH,16
+mov DL, 030
+int 10h
+mov ah,09
+mov al,7CH
+mov bh,00
+mov bl,00001001b
+mov cx,1
+int 10h 
+
+mov AH, 02h
+mov BH, 00
+mov DH, 15
+mov DL, 030
+int 10h
+mov ah,09
+mov al,7CH
+mov bh,00
+mov bl,00001001b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 14
+mov DL, 030
+int 10h
+mov ah,09
+mov al,7CH
+mov bh,00
+mov bl,00001001b
+mov cx,1
+int 10h            
+
+mov AH, 02h
+mov BH, 00
+mov DH, 13
+mov DL, 030
+int 10h
+mov ah,09
+mov al,7CH
+mov bh,00
+mov bl,00001001b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 21
+mov DL, 045
+int 10h
+mov ah,09
+mov al,7CH
+mov bh,00
+mov bl,00001001b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 20
+mov DL, 045
+int 10h
+mov ah,09
+mov al,7CH
+mov bh,00
+mov bl,00001001b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 19
+mov DL, 045
+int 10h
+mov ah,09
+mov al,7CH
+mov bh,00
+mov bl,00001001b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 18
+mov DL, 045
+int 10h
+mov ah,09
+mov al,7CH
+mov bh,00
+mov bl,00001001b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 17
+mov DL, 045
+int 10h
+mov ah,09
+mov al,7CH
+mov bh,00
+mov bl,00001001b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 16
+mov DL, 045
+int 10h
+mov ah,09
+mov al,7CH
+mov bh,00
+mov bl,00001001b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 15
+mov DL, 045
+int 10h
+mov ah,09
+mov al,7CH
+mov bh,00
+mov bl,00001001b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 14
+mov DL, 045
+int 10h
+mov ah,09
+mov al,7CH
+mov bh,00
+mov bl,00001001b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 13
+mov DL, 045
+int 10h
+mov ah,09
+mov al,7CH
+mov bh,00
+mov bl,00001001b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 12
+mov DL, 030
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00001001b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 12
+mov DL, 031
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00001001b
+mov cx,1
+int 10h 
+
+mov AH, 02h
+mov BH, 00
+mov DH, 12
+mov DL, 032
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00001001b
+mov cx,1
+int 10h 
+
+mov AH, 02h
+mov BH, 00
+mov DH, 12
+mov DL, 033
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00001001b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 12
+mov DL, 034
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00001001b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 12
+mov DL, 035
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00001001b
+mov cx,1
+int 10h 
+
+mov AH, 02h
+mov BH, 00
+mov DH, 12
+mov DL, 036
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00001001b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 12
+mov DL, 037
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00001001b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 12
+mov DL, 038
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00001001b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 12
+mov DL, 039
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00001001b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 12
+mov DL, 040
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00001001b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 12
+mov DL, 041
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00001001b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 12
+mov DL, 042
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00001001b
+mov cx,1
+int 10h 
+
+mov AH, 02h
+mov BH, 00
+mov DH, 12
+mov DL, 043
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00001001b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 12
+mov DL, 044
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00001001b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 12
+mov DL, 045
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00001001b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 12
+mov DL, 030
+int 10h
+mov ah,09
+mov al,2FH
+mov bh,00
+mov bl,00000011b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 11
+mov DL, 031
+int 10h
+mov ah,09
+mov al,2FH
+mov bh,00
+mov bl,00000011b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 10
+mov DL, 032
+int 10h
+mov ah,09
+mov al,2FH
+mov bh,00
+mov bl,00000011b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 09
+mov DL, 033
+int 10h
+mov ah,09
+mov al,2FH
+mov bh,00
+mov bl,00000011b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 08
+mov DL, 034
+int 10h
+mov ah,09
+mov al,2FH
+mov bh,00
+mov bl,00000011b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 07
+mov DL, 035
+int 10h
+mov ah,09
+mov al,2FH
+mov bh,00
+mov bl,00000011b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 06
+mov DL, 036
+int 10h
+mov ah,09
+mov al,2FH
+mov bh,00
+mov bl,00000011b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 05
+mov DL, 037
+int 10h
+mov ah,09
+mov al,2FH
+mov bh,00
+mov bl,00000011b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 11
+mov DL, 045
+int 10h
+mov ah,09
+mov al,92
+mov bh,00
+mov bl,00000011b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 10
+mov DL, 044
+int 10h
+mov ah,09
+mov al,92
+mov bh,00
+mov bl,00000011b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 09
+mov DL, 043
+int 10h
+mov ah,09
+mov al,92
+mov bh,00
+mov bl,00000011b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 08
+mov DL, 042
+int 10h
+mov ah,09
+mov al,92
+mov bh,00
+mov bl,00000011b
+mov cx,1
+int 10h 
+
+mov AH, 02h
+mov BH, 00
+mov DH, 07
+mov DL, 041
+int 10h
+mov ah,09
+mov al,92
+mov bh,00
+mov bl,00000011b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 06
+mov DL, 040
+int 10h
+mov ah,09
+mov al,92
+mov bh,00
+mov bl,00000011b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 05
+mov DL, 039
+int 10h
+mov ah,09
+mov al,92
+mov bh,00
+mov bl,00000011b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 15
+mov DL, 044
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00000100b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 15
+mov DL, 043
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00000100b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 15
+mov DL, 042
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00000100b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 15
+mov DL, 041
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00000100b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 15
+mov DL, 040
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00000100b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 15
+mov DL, 036
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00000100b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 15
+mov DL, 035
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00000100b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 15
+mov DL, 034
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00000100b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 15
+mov DL, 033
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00000100b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 15
+mov DL, 032
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00000100b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 15
+mov DL, 032
+int 10h
+mov ah,09
+mov al,7CH
+mov bh,00
+mov bl,00000100b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 14
+mov DL, 032
+int 10h
+mov ah,09
+mov al,7CH
+mov bh,00
+mov bl,00000100b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 13
+mov DL, 032
+int 10h
+mov ah,09
+mov al,7CH
+mov bh,00
+mov bl,00000100b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 13
+mov DL, 033
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00000100b
+mov cx,1
+int 10h 
+
+mov AH, 02h
+mov BH, 00
+mov DH, 13
+mov DL, 034
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00000100b
+mov cx,1
+int 10h 
+
+mov AH, 02h
+mov BH, 00
+mov DH, 13
+mov DL, 035
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00000100b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 13
+mov DL, 036
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00000100b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 13
+mov DL, 037
+int 10h
+mov ah,09
+mov al,7CH
+mov bh,00
+mov bl,00000100b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 14
+mov DL, 037
+int 10h
+mov ah,09
+mov al,7CH
+mov bh,00
+mov bl,00000100b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 15
+mov DL, 037
+int 10h
+mov ah,09
+mov al,7CH
+mov bh,00
+mov bl,00000100b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 15
+mov DL, 040
+int 10h
+mov ah,09
+mov al,7CH
+mov bh,00
+mov bl,00000100b
+mov cx,1
+int 10h 
+
+mov AH, 02h
+mov BH, 00
+mov DH, 14
+mov DL, 040
+int 10h
+mov ah,09
+mov al,7CH
+mov bh,00
+mov bl,00000100b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 13
+mov DL, 040
+int 10h
+mov ah,09
+mov al,7CH
+mov bh,00
+mov bl,00000100b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 13
+mov DL, 041
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00000100b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 13
+mov DL, 042
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00000100b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 13
+mov DL, 043
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00000100b
+mov cx,1
+int 10h   
+
+mov AH, 02h
+mov BH, 00
+mov DH, 13
+mov DL, 044
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00000100b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 13
+mov DL, 044
+int 10h
+mov ah,09
+mov al,7CH
+mov bh,00
+mov bl,00000100b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 14
+mov DL, 044
+int 10h
+mov ah,09
+mov al,7CH
+mov bh,00
+mov bl,00000100b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 15
+mov DL, 044
+int 10h
+mov ah,09
+mov al,7CH
+mov bh,00
+mov bl,00000100b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 15
+mov DL, 042
+int 10h
+mov ah,09
+mov al,7CH
+mov bh,00
+mov bl,00000100b
+mov cx,1
+int 10h 
+
+mov AH, 02h
+mov BH, 00
+mov DH, 14
+mov DL, 042
+int 10h
+mov ah,09
+mov al,7CH
+mov bh,00
+mov bl,00000100b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 13
+mov DL, 042
+int 10h
+mov ah,09
+mov al,7CH
+mov bh,00
+mov bl,00000100b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 14
+mov DL, 043
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00000100b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 14
+mov DL, 043
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00000100b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 14
+mov DL, 042
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00000100b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 14
+mov DL, 041
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00000100b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 14
+mov DL, 036
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00000100b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 14
+mov DL, 036
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00000100b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 14
+mov DL, 035
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00000100b
+mov cx,1
+int 10h
+        
+mov AH, 02h
+mov BH, 00
+mov DH, 14
+mov DL, 034
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00000100b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 14
+mov DL, 033
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00000100b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 13
+mov DL, 035
+int 10h
+mov ah,09
+mov al,7CH
+mov bh,00
+mov bl,00000100b
+mov cx,1
+int 10h 
+
+mov AH, 02h
+mov BH, 00
+mov DH, 14
+mov DL, 035
+int 10h
+mov ah,09
+mov al,7CH
+mov bh,00
+mov bl,00000100b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 15
+mov DL, 035
+int 10h
+mov ah,09
+mov al,7CH
+mov bh,00
+mov bl,00000100b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 21
+mov DL, 034
+int 10h
+mov ah,09
+mov al,7CH
+mov bh,00
+mov bl,00000010b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 20
+mov DL, 034
+int 10h
+mov ah,09
+mov al,7CH
+mov bh,00
+mov bl,00000010b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 19
+mov DL, 034
+int 10h
+mov ah,09
+mov al,7CH
+mov bh,00
+mov bl,00000010b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 18
+mov DL, 034
+int 10h
+mov ah,09
+mov al,7CH
+mov bh,00
+mov bl,00000010b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 17
+mov DL, 034
+int 10h
+mov ah,09
+mov al,7CH
+mov bh,00
+mov bl,00000010b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 17
+mov DL, 035
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00000010b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 17
+mov DL, 036
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00000010b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 17
+mov DL, 037
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00000010b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 17
+mov DL, 038
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00000010b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 17
+mov DL, 039
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00000010b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 17
+mov DL, 040
+int 10h
+mov ah,09
+mov al,2DH
+mov bh,00
+mov bl,00000010b
+mov cx,1
+int 10h 
+
+mov AH, 02h
+mov BH, 00
+mov DH, 18
+mov DL, 041
+int 10h
+mov ah,09
+mov al,7CH
+mov bh,00
+mov bl,00000010b
+mov cx,1
+int 10h 
+
+mov AH, 02h
+mov BH, 00
+mov DH, 17
+mov DL, 041
+int 10h
+mov ah,09
+mov al,7CH
+mov bh,00
+mov bl,00000010b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 18
+mov DL, 041
+int 10h
+mov ah,09
+mov al,7CH
+mov bh,00
+mov bl,00000010b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 19
+mov DL, 041
+int 10h
+mov ah,09
+mov al,7CH
+mov bh,00
+mov bl,00000010b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 20
+mov DL, 041
+int 10h
+mov ah,09
+mov al,7CH
+mov bh,00
+mov bl,00000010b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 21
+mov DL, 041
+int 10h
+mov ah,09
+mov al,7CH
+mov bh,00
+mov bl,00000010b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 01
+mov DL, 012
+int 10h
+mov ah,09
+mov al,2AH
+mov bh,00
+mov bl,00000110b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 02
+mov DL, 013
+int 10h
+mov ah,09
+mov al,2AH
+mov bh,00
+mov bl,00000110b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 05
+mov DL, 055
+int 10h
+mov ah,09
+mov al,2AH
+mov bh,00
+mov bl,00000110b
+mov cx,1
+int 10h 
+
+mov AH, 02h
+mov BH, 00
+mov DH, 03
+mov DL, 046
+int 10h
+mov ah,09
+mov al,2AH
+mov bh,00
+mov bl,00000110b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 02
+mov DL, 022
+int 10h
+mov ah,09
+mov al,2AH
+mov bh,00
+mov bl,00000110b
+mov cx,1
+int 10h 
+
+mov AH, 02h
+mov BH, 00
+mov DH, 01
+mov DL, 030
+int 10h
+mov ah,09
+mov al,2AH
+mov bh,00
+mov bl,00000110b
+mov cx,1
+int 10h 
+
+mov AH, 02h
+mov BH, 00
+mov DH, 04
+mov DL, 040
+int 10h
+mov ah,09
+mov al,2AH
+mov bh,00
+mov bl,00000110b
+mov cx,1
+int 10h
+
+mov AH, 02h
+mov BH, 00
+mov DH, 03
+mov DL, 060
+int 10h
+mov ah,09
+mov al,2AH
+mov bh,00
+mov bl,00000110b
+mov cx,1
+int 10h           
+        
+
+
+
+
+
+
+
+
+
